6b73bd2598e557062dd703b6a9eb7a5c5b749c5e
Erik Romunde <eromunde@hotmail.com>
2023-03-01 05:35:56 +0100

    The start of HaiDesu! Image Managing App.

---
+++ haidesu-rest/src/test/java/com/haidesu/HaiDesuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haidesu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaiDesuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
