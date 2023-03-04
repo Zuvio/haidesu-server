d663a8aed9f36f5ab8850ec80e88213d9a7c68e8
Erik Romunde <eromunde@hotmail.com>
2023-03-04 12:56:25 +0100

    - server url ingekort van /api/v1 - comments toegevoegd op @service classes

--- haidesu-rest/src/main/java/com/haidesu/controller/UsersApiDelegateImpl.java
+++ haidesu-rest/src/main/java/com/haidesu/controller/UsersApiDelegateImpl.java
@@ -8,13 +8,15 @@ import org.springframework.stereotype.Service;
 
 /**
  * <p>
- * Deze class UsersApiDelegateImpl implementeert de UsersApiDelegate interface gegenereerd door OpenAPI.
- * Dit is niet de RestController class die calls afhandelt, dit is de @Service class die de UsersApiDelegate
- * methods override voor correcte implementatie.
+ * Deze class UsersApiDelegateImpl implementeert de {@link UsersApiDelegate} interface gegenereerd door OpenAPI.
+ * Dit is <b>niet</b> de {@link org.springframework.stereotype.Controller} class die calls afhandelt,
+ * dit is de {@link org.springframework.stereotype.Service} class die de {@link UsersApiDelegate} methods override
+ * voor correcte implementatie.
  * </p>
  * <p>
- * Deze implementatie class met achterliggende services om business logic uit te voeren zou altijd moeten blijven
- * werken, ook al verandert de OpenAPI Specification. Dat is het doel van de OpenAPI delegatePattern.
+ * Deze implementatie class met achterliggende {@link UserService} om business logic uit te voeren,
+ * zou altijd moeten blijven werken, ook al verandert de OpenAPI Specification.
+ * Dat is het doel van de OpenAPI delegatePattern=true.
  * </p>
  */
 
--- haidesu-rest/src/main/java/com/haidesu/service/UserService.java
+++ haidesu-rest/src/main/java/com/haidesu/service/UserService.java
@@ -8,6 +8,10 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class UserService {
 
+    /**
+     * Deze database variabele mocked een database connectie.
+     * Werkelijke persistence implementatie volgt later.
+     */
     private final ConcurrentHashMap<Long, User> database = new ConcurrentHashMap<>();
 
     public User addUser(User user) {
@@ -16,6 +20,8 @@ public class UserService {
         newUser.setPassword(user.getPassword());
         newUser.setEmail(user.getEmail());
         newUser.setSecretword(user.getSecretword());
+
+        // mocked het toevoegen aan de 'database' en returned de user zoals de db dat zou doen
         database.put(1L, newUser);
         return newUser;
     }
