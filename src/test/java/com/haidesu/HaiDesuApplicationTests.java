4f40f5ed3d99081f274472660e92ad414a0bf353
Erik Romunde <eromunde@hotmail.com>
2023-03-05 17:51:40 +0100

    HaiDesu 0.1.0: - alle modules in één main module gezet voor beter overzicht

---
+++ src/test/java/com/haidesu/HaiDesuApplicationTests.java
@@ -0,0 +1,14 @@
+//package com.haidesu;
+//
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HaiDesuApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
